a8bab6fe946f1c845061afa162ef26baf1e9300c
Chris Molozian <chris@heroiclabs.com>
2019-06-20 17:03:58 +0100

    Format Go code.

--- example.go
+++ example.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-  "context"
-  "database/sql"
+	"context"
+	"database/sql"
 
-  "github.com/heroiclabs/nakama/runtime"
+	"github.com/heroiclabs/nakama/runtime"
 )
 
 func InitModule(ctx context.Context, logger runtime.Logger, db *sql.DB, nk runtime.NakamaModule, initializer runtime.Initializer) error {
-  logger.Info("module loaded")
-  return nil
+	logger.Info("module loaded")
+	return nil
 }
